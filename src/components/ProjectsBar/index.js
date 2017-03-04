6a4a7c255e71fc5ac776c0a1de982d94b7d82c9b
Charanjit Singh <ckhabra@gmail.com>
2017-03-04 17:59:37 +0530

    Props to ProjectsBar

--- src/components/ProjectsBar/index.js
+++ src/components/ProjectsBar/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {
+  PropTypes
+} from 'react';
 // eslint-disable-next-line import/no-unassigned-import, lines-around-comment
 import './styles.scss';
 
-const ProjectsBar = function ProjectsBar () {
+const ProjectsBar = function ProjectsBar (props) {
   return (
     <div className='sidebar__container'>
       <h2 className='sidebar__heading'>Projects <i className='sidebar__add-project' /></h2>
 
       <ul className='sidebar__projects-list'>
-        <li className='sidebar__project'>Workcave</li>
-        <li className='sidebar__project'>PCCHD</li>
+        {
+          props.projects.map((project) => {
+            return (
+              <li className='sidebar__project' key={project.id}>{project.title}</li>
+            );
+          })
+        }
       </ul>
     </div>
   );
+};
+
+ProjectsBar.propTypes = {
+  projects: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string
+  })).isRequired
 };
 
 export default ProjectsBar;
--- src/views/App/index.js
+++ src/views/App/index.js
@@ -5,13 +5,26 @@ import ProjectsBar from './../../components/ProjectsBar';
 // eslint-disable-next-line import/no-unassigned-import, lines-around-comment
 import './styles.scss';
 
+const projects = [{
+  id: 1,
+  title: 'Workcave'
+}, {
+  id: 2,
+  title: 'PCCHD'
+}, {
+  id: 3,
+  title: 'Applaudience'
+}];
+
 /* eslint-disable react/prefer-stateless-function, class-methods-use-this */
 class App extends Component {
   render () {
     return (
       <div className='container'>
         <div className='sidebar'>
-          <ProjectsBar />
+          <ProjectsBar
+            projects={projects}
+          />
         </div>
 
         <div className='content'>Content</div>
