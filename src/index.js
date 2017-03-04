d701144e0887078172a7ce4e38123e5b5090d083
Charanjit Singh <ckhabra@gmail.com>
2017-03-04 16:29:52 +0530

    First view
    - Mail layout with styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello PCCHD!');
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './views/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'));
---
+++ src/views/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// eslint-disable-next-line import/no-unassigned-import, lines-around-comment
+import './styles.scss';
+
+const App = function App () {
+  return (
+    <div className='container'>
+      <div className='sidebar'>Sidebar</div>
+      <div className='content'>Content</div>
+    </div>
+  );
+};
+
+export default App;
